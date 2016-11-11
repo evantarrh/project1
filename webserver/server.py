df517e589a78bc7652f46e88ae1f5e0a9997dcf6
Evan Tarrh <evantarrh@gmail.com>
2016-11-10 19:44:43 -0500

    Implement login/user sessions

--- webserver/server.py
+++ webserver/server.py
@@ -19,26 +19,16 @@ import os
 import config
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
-from flask import Flask, request, render_template, g, redirect, Response
+from flask import Flask, request, render_template, g, redirect, Response, session
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
 
-#
-# This line creates a database engine that knows how to connect to the URI above
-#
 engine = create_engine(config.DATABASE_URI)
 
 
 @app.before_request
 def before_request():
-  """
-  This function is run at the beginning of every web request 
-  (every time you enter an address in the web browser).
-  We use it to setup a database connection that can be used throughout the request
-
-  The variable g is globally accessible
-  """
   try:
     g.conn = engine.connect()
   except:
@@ -57,107 +47,88 @@ def teardown_request(exception):
   except Exception as e:
     pass
 
-
-#
-# @app.route is a decorator around index() that means:
-#   run index() whenever the user tries to access the "/" path using a GET request
-#
-# If you wanted the user to go to e.g., localhost:8111/foobar/ with POST or GET then you could use
-#
-#       @app.route("/foobar/", methods=["POST", "GET"])
-#
-# PROTIP: (the trailing / in the path is important)
-# 
-# see for routing: http://flask.pocoo.org/docs/0.10/quickstart/#routing
-# see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
-#
 @app.route('/')
 def index():
-  """
-  request is a special object that Flask provides to access web request information:
+  current_user = None
 
-  request.method:   "GET" or "POST"
-  request.form:     if the browser submitted a form, this contains the data in the form
-  request.args:     dictionary of URL arguments e.g., {a:1, b:2} for http://localhost?a=1&b=2
+  if 'username' in session:
+    current_user = session['username']
 
-  See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
-  """
-
-  # DEBUG: this is debugging code to see what request looks like
-  print request.args
-
-
-  #
-  # example of a database query
-  #
   cursor = g.conn.execute("SELECT username FROM Account")
   names = []
   for result in cursor:
     names.append(result[0])  # can also be accessed using result[0]
   cursor.close()
 
-  #
-  # Flask uses Jinja templates, which is an extension to HTML where you can
-  # pass data to a template and dynamically generate HTML based on the data
-  # (you can think of it as simple PHP)
-  # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
-  #
-  # You can see an example template in templates/index.html
-  #
-  # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be 
-  # accessible as a variable in index.html:
-  #
-  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-  #     <div>{{data}}</div>
-  #     
-  #     # creates a <div> tag for each element in data
-  #     # will print: 
-  #     #
-  #     #   <div>grace hopper</div>
-  #     #   <div>alan turing</div>
-  #     #   <div>ada lovelace</div>
-  #     #
-  #     {% for n in data %}
-  #     <div>{{n}}</div>
-  #     {% endfor %}
-  #
-  context = dict(data = names)
+  context = dict(data=names, current_user=current_user)
 
-
-  #
-  # render_template looks in the templates/ folder for files.
-  # for example, the below file reads template/index.html
-  #
   return render_template("index.html", **context)
-
-#
-# This is an example of a different path.  You can see it at
-# 
-#     localhost:8111/another
-#
-# notice that the functio name is another() rather than index()
-# the functions for each app.route needs to have different names
-#
-@app.route('/another')
-def another():
-  return render_template("anotherfile.html")
-
 
 # Example of adding new data to the database
 @app.route('/add', methods=['POST'])
 def add():
   name = request.form['name']
   print name
-  cmd = 'INSERT INTO test(name) VALUES (:name1), (:name2)';
-  g.conn.execute(text(cmd), name1 = name, name2 = name);
+  cmd = 'INSERT INTO test(name) VALUES (:name1), (:name2)'
+  g.conn.execute(text(cmd), name1 = name, name2 = name)
   return redirect('/')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    abort(401)
-    this_is_never_executed()
+  if request.method == 'GET':
+    return render_template("login.html")
+
+  username = request.form['username']
+
+  q = "SELECT password FROM Account WHERE username = '{}'".format(username)
+  cursor = g.conn.execute(q)
+  results = []
+
+  for result in cursor:
+    results.append(result[0])  # can also be accessed using result[0]
+  cursor.close()
+
+  # If username or password is incorrect
+  if (len(results) == 0 or request.form['pw'] != results[0]):
+    return render_template("login.html", error=True)
+
+  session['username'] = username
+  return redirect('/')
+
+@app.route('/<username>', methods=['GET'])
+def view_profile(username):
+  posts, uids, followers, channels = [], [], [], []
+
+  uid_q = """SELECT uid FROM Account WHERE username = %s"""
+  cursor = g.conn.execute(uid_q, (username))
+
+  for result in cursor:
+    uids.append(result[0])
+
+  if len(uids) != 1:
+    abort(500)
+
+  uid = uids[0] # lol
+
+  posts_q = """SELECT * FROM Posted
+               WHERE Posted.uid = %s
+               ORDER BY Posted.time_created DESC
+               LIMIT 20"""
+
+  cursor = g.conn.execute(posts_q, (uid))
+
+  for result in cursor:
+    posts.append(result[-1])
+  cursor.close()
+
+  return render_template("user.html", posts=posts)
+
+@app.route('/logout')
+def logout():
+  # remove the username from the session if it's there
+  session.pop('username', None)
+  return redirect('/')
 
 
 if __name__ == "__main__":
@@ -183,7 +154,8 @@ if __name__ == "__main__":
 
     HOST, PORT = host, port
     print "running on %s:%d" % (HOST, PORT)
-    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+    app.secret_key = config.SESSION_KEY
+    app.run(host=HOST, port=PORT, debug=True, threaded=threaded)
 
 
   run()
